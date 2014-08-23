0622378a5b648bfdf1fb0d2ff91aea03d573c537
Hitesh Kanwar <erhitesh_2010@yahoo.co.in>
2014-08-23 15:32:28 +0530

    changes

--- post/settings.py
+++ post/settings.py
@@ -50,7 +50,6 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'registration',
     'django_extensions',
     'post',
     'django.contrib.humanize',
