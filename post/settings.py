ec1ba04872b0c8ebe169feeee8f5c67ac62f4383
Hitesh Kanwar <erhitesh_2010@yahoo.co.in>
2014-08-25 19:09:08 +0530

    changes

--- post/settings.py
+++ post/settings.py
@@ -47,7 +47,10 @@ ALLOWED_HOSTS = ['localhost', '127.0.0.1', ]
 INSTALLED_APPS = (
     'django.contrib.admin',
     'django.contrib.auth',
-    'django.contrib.contenttypes',,
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
     'django_extensions',
     'post',
     'django.contrib.humanize',
@@ -65,6 +68,7 @@ MIDDLEWARE_CLASSES = (
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 )
 
