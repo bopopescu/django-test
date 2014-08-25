b932940edf805bb95ab2586c2b38255b9ec48196
Hitesh Kanwar <erhitesh_2010@yahoo.co.in>
2014-08-25 16:59:19 +0530

    changes

--- post/settings.py
+++ post/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/1.6/ref/settings/
 import os
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 from PIL import Image
-sys.modules['Image'] = Image
+
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.6/howto/deployment/checklist/
 
