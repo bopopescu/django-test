a7f26624084660e3a8471f4f0980ea7e30dd22bf
Hitesh Kanwar <erhitesh_2010@yahoo.co.in>
2014-08-25 18:48:24 +0530

    changes

--- post/settings.py
+++ post/settings.py
@@ -10,6 +10,8 @@ https://docs.djangoproject.com/en/1.6/ref/settings/
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
+os.environ['DJANGO_SETTINGS_MODULE'] = 'settings'
+
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 from PIL import Image
 
