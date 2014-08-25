d830d56b1a02b0826c20fd6b01ed7bc005b40393
Hitesh Kanwar <erhitesh_2010@yahoo.co.in>
2014-08-25 18:26:00 +0530

    changes

--- post/urls.py
+++ post/urls.py
@@ -1,4 +1,4 @@
-from django.conf.urls.defaults import patterns, url
+from django.conf.urls import patterns, url, include
 from django.contrib.auth.views import login, logout
 from django.contrib import admin
 admin.autodiscover()
