402fc23135482c30f98cd92e352a6e120342ff6b
Hitesh Kanwar <erhitesh_2010@yahoo.co.in>
2014-08-26 16:14:13 +0530

    pil import remove

--- userprofile/models.py
+++ userprofile/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.db.models.signals import post_save
 #from django.utils.image import Image
-import PIL.Image as Image
+#import PIL.Image as Image
 #from registration.signals import user_registered
 import pdb;
 
