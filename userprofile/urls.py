19dafaebd26694d6f85fb76e7a2bea6a0122c5d2
Hitesh Kanwar <erhitesh_2010@yahoo.co.in>
2014-08-26 19:50:48 +0530

    changes

--- userprofile/urls.py
+++ userprofile/urls.py
@@ -1,5 +1,5 @@
 from django.conf.urls import patterns, include, url
-from registration.backends.default.views import RegistrationView
+#from registration.backends.default.views import RegistrationView
 from userprofile.forms import UserRegistrationForm
 from userprofile.models import UserProfile
 
@@ -8,7 +8,7 @@ urlpatterns = patterns('',
  
     # ...
     
-    url(r'^accounts/register/$',RegistrationView,{'form_class' : UserRegistrationForm},name='registration_register'),
+   # url(r'^accounts/register/$',RegistrationView,{'form_class' : UserRegistrationForm},name='registration_register'),
    
  
 )
