fe85873317d66cdfe7989cd3fd4fc9f5ae41e248
Hitesh Kanwar <erhitesh_2010@yahoo.co.in>
2014-09-08 19:02:22 +0530

    setting file changes

--- userprofile/urls.py
+++ userprofile/urls.py
@@ -1,5 +1,5 @@
 from django.conf.urls import patterns, include, url
-#from registration.backends.default.views import RegistrationView
+from registration.backends.default.views import RegistrationView
 from userprofile.forms import UserRegistrationForm
 from userprofile.models import UserProfile
 
